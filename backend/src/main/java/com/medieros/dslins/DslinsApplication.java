ea6f21617de03556863a150558c5763044c6c57d
Marcelo Lins de Medeiros <@marceloLins-code>
2021-05-04 15:12:09 -0300

    Project created

---
+++ backend/src/main/java/com/medieros/dslins/DslinsApplication.java
@@ -0,0 +1,13 @@
+package com.medieros.dslins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslinsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/medieros/dslins/DslinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medieros.dslins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
