ba0ca3790aec4a3b52143310882a839dfbe0f51c
Marcelo Lins de Medeiros <@marceloLins-code>
2021-05-06 12:53:18 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+        <div className="container">
+          <p className="text-light">app developed by <a href="https://github.com/marceloLins-code" target="_blank" rel="noreferrer">Marcelo Lins</a></p>
+          <p className="text-light"><small><strong>Semana Spring React</strong><br/>
+            Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+        </div>
+      </footer>
+
+    );
+}
+
+export default Footer;
