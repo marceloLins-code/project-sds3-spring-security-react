65484805e0edd88f8e90560c32aca93c05f5c8a5
Marcelo Lins de Medeiros <@marceloLins-code>
2021-05-07 16:39:27 -0300

    Security Config

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -14,37 +14,37 @@ const DataTable = () => {
         <tbody>
             <tr>
                 <td>22/04/2021</td>
-                <td>Barry Allen</td>
-                <td>34</td>
-                <td>25</td>
-                <td>15017.00</td>
+                <td>Renato Alves</td>
+                <td>23</td>
+                <td>33</td>
+                <td>33411.00</td>
             </tr>
             <tr>
                 <td>22/04/2021</td>
-                <td>Barry Allen</td>
+                <td>Marcelo Lins</td>
                 <td>34</td>
                 <td>25</td>
-                <td>15017.00</td>
+                <td>457822.00</td>
             </tr>
             <tr>
                 <td>22/04/2021</td>
-                <td>Barry Allen</td>
-                <td>34</td>
-                <td>25</td>
-                <td>15017.00</td>
+                <td>Marcos Tulio</td>
+                <td>22</td>
+                <td>43</td>
+                <td>61122.00</td>
             </tr>
             <tr>
                 <td>22/04/2021</td>
-                <td>Barry Allen</td>
-                <td>34</td>
-                <td>25</td>
-                <td>15017.00</td>
+                <td>Eliane Allen</td>
+                <td>61</td>
+                <td>44</td>
+                <td>45255.00</td>
             </tr>
             <tr>
                 <td>22/04/2021</td>
-                <td>Barry Allen</td>
-                <td>34</td>
-                <td>25</td>
+                <td>Bryan Lex</td>
+                <td>12</td>
+                <td>44</td>
                 <td>15017.00</td>
             </tr>
         </tbody>
